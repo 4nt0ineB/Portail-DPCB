de88440dfe86a16368fbefdb5abf3818339426c4
cydaw6 <cydaw6@gmail.com>
2020-09-13 01:01:00 +0200

    test

---
+++ deploy.php
@@ -0,0 +1,9 @@
+<?php
+
+// Use in the “Post-Receive URLs” section of your GitHub repo.
+
+if ( $_POST['payload'] ) {
+shell_exec( ‘cd /var/www/portail-dpcb/ && git reset –hard HEAD && git pull’ );
+}
+
+?>hi
