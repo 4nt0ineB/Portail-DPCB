efd4dbafb21226b05372154b4f429e5fa0ac05f6
cydaw6 <cydaw6@gmail.com>
2020-10-01 00:03:16 +0200

    changement format montant

--- datatable.php
+++ datatable.php
@@ -90,7 +90,7 @@
                             <td>' . $r['reseau'] . '</td>
                             <td>' . $r['num_dossier'] . '</td>
                             <td>' . $r['libelle_devise'] . '</td>
-                            <td>' . $r['montant_impaye'] . '</td>
+                            <td>' . number_format($r['montant_impaye'], 2, '.', ' ') . '</td>
                             <td>' . $r['libelle'] . '</td>
                             </tr>';
                         }
