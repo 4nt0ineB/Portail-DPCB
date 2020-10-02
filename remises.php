ffb7e3488f7ff1d7b2817a7cff00f677ed70481b
Zkimi <20417186+zkimi@users.noreply.github.com>
2020-10-02 19:59:44 +0200

    remises V1

--- remises.php
+++ remises.php
@@ -1,3 +1,11 @@
+
+<?php
+session_start();
+if (!isset($_SESSION["logged"])) header("location: index.php"); //Vérifie si une session est en cours sinon renvoi à l'index
+require_once("includes/mysql.php");
+include('includes/fonctions.php')
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -11,6 +19,31 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pikaday/1.6.1/css/pikaday.min.css">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1cQRFi3dgfSVKpc1B9idTEuN3cBScszNHP9silYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+
+    <!-- CDN Datatable -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css">
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
+
+    <!-- plugins Datatable -->
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/1.6.4/js/dataTables.buttons.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.bootstrap4.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.html5.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.print.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.colVis.min.js"></script>
+
+    <!-- css pour l'option "column visibility" des plugins -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.4/css/buttons.bootstrap4.min.css">
 </head>
 
 <body>
@@ -52,14 +85,14 @@
                                                 </thead>
                                                 <tbody>
                                                     <tr>
-                                                        <td><label>N° SIREN :&nbsp;</label><input type="text"></td>
-                                                        <td><label style="font-style: normal;">Raison sociale
+                                                        <td><label>N° SIREN :&nbsp;</label><input type="text" name="siren"></td>
+                                                        <td><label style="font-style: normal;" name="raison">Raison sociale
                                                                 :&nbsp;<input type="text"></label></td>
                                                         <td></td>
                                                     </tr>
                                                     <tr>
-                                                        <td><label>Date de début :&nbsp;</label><input type="date"></td>
-                                                        <td><label>Date de fin :&nbsp;</label><input type="date"></td>
+                                                        <td><label>Date de début :&nbsp;</label><input type="date" name="datedebut"></td>
+                                                        <td><label>Date de fin :&nbsp;</label><input type="date" name="datefin"></td>
                                                         <td style="text-align: center;"><button class="btn btn-primary"
                                                                 type="button"
                                                                 style="text-align: center;background: rgba(255,255,255,0);color: rgb(0,0,0);box-shadow: 0px 0px 3px;border-style: none;">Rechercher</button>
@@ -71,34 +104,7 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="row">
-                                            <div class="col">
-                                                <div class="form-check text-right"><input class="form-check-input"
-                                                        type="radio" id="formCheck-1" name="choicetab"
-                                                        value="tabdetaille"><label class="form-check-label"
-                                                        for="formCheck-1">Tableau détaillé</label></div>
-                                                <div class="form-check text-right"><input class="form-check-input"
-                                                        type="radio" id="formCheck-2" checked="" name="choicetab"
-                                                        value="tabresume"><label class="form-check-label text-right"
-                                                        for="formCheck-2">Tableau résumé</label></div>
-                                                <div class="btn-toolbar"
-                                                    style="margin-bottom: 10px;float: right;border-style: none;">
-                                                    <div class="btn-group" role="group"
-                                                        style="border-width: 0px;border-style: none;"><button
-                                                            class="btn btn-primary" type="button"
-                                                            style="color: rgb(0,0,0);background: rgb(255,255,255);border: 1px solid rgb(0,0,0);border-bottom-left-radius: 10px;border-top-left-radius: 10px;box-shadow: 0px 0px 2px;">XLS</button>
-                                                        <button class="btn btn-primary" type="button"
-                                                            style="background: rgb(255,255,255);color: rgb(0,0,0);border-style: solid;border-color: rgb(0,0,0);box-shadow: 0px 0px 3px;">CSV</button><button
-                                                            class="btn btn-primary" type="button"
-                                                            style="background: rgb(255,255,255);color: rgb(0,0,0);border-color: rgb(0,0,0);border-top-right-radius: 10px;border-bottom-right-radius: 10px;box-shadow: 0px 0px 3px;">PDF</button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+
                                 <div class="row">
                                     <div class="col">
                                          <table id="datatable" class="table table-striped table-bordered">
@@ -116,34 +122,35 @@
                     </thead>
                     <tbody>
                         <?php
-                        /*while ($r = $resultat->fetch()) {
-                            echo '<tr>
-                            <td>' . $r['siren'] . '</td>
-                            <td>' . $r['date_vente'] . '</td>
-                            <td>' . $r['date_remise'] . '</td>
-                            <td>' . $r['num_carte'] . '</td>
-                            <td>' . $r['reseau'] . '</td>
-                            <td>' . $r['num_dossier'] . '</td>
-                            <td>' . $r['libelle_devise'] . '</td>
-                            <td>' . number_format($r['montant_impaye'], 2, '.', ' ') . '</td>
-                            <td>' . $r['libelle'] . '</td>
-                            </tr>';
-                        }
-                        */
+
+                        $id = $_SESSION["logged"];
+                        $requete = "SELECT * FROM REMISE,DEVISE WHERE id_client=$id AND REMISE.id_devise = DEVISE.id_devise;";
+
+                        if (isset($_POST['siren'])) $siren = secure_sqlformat($_POST['siren']);
+                        if (isset($_POST['raison'])) $raison = secure_sqlformat(strip_tags($_POST['raison']));
+                        if (isset($_POST['datedebut']))  $datedebut = $_POST['datedebut'];
+                        if (isset($_POST['datefin']))  $datefin = $_POST['datefin'];
+
+
+                        $resultat = $db->query($requete);
 
                         while ($r = $resultat->fetch()){
-                                                            echo '<tr
-                                                            <td>' . "" . '</td>
+                                                            echo '<tr>
                                                             <td>' . $r['siren'] . '</td>
                                                             <td>' . $r['raison'] . '</td>
-                                                            <td>' . $r['nombreTransaction'] . '</td>
-                                                            <td>EUR</td>
-                                                            <td>' . $r['montantTransaction'] . '</td>
+                                                            <td>' . $r['num_remise'] . '</td>
+                                                            <td>' . $r['date_traitement'] . '</td>
+                                                            <td>' . $r['nbr_transaction'] . '</td>
+                                                            <td>' . $r['libelle_devise'] . '</td>
+                                                            <td>' . $r['montant_remise'] . '</td>
+                                                            <td>' . $r['sens'] . '</td>
                                                             </tr>';
                                                         }
                         ?>
                     </tbody>
                 </table>
+                <script type=" text/javascript" charset="utf8" src="assets/js/tableplugins.js">
+    </script>
                                         </div>
                                     </div>
                                 </div>
