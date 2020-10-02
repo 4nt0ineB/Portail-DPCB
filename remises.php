d485fbbae8c050e387e84978c77f557a73d7a242
cydaw6 <cydaw6@gmail.com>
2020-10-02 23:13:47 +0200

    problème de responsivité réduit à NEANT

--- remises.php
+++ remises.php
@@ -1,4 +1,3 @@
-
 <?php
 session_start();
 if (!isset($_SESSION["logged"])) header("location: index.php"); //Vérifie si une session est en cours sinon renvoi à l'index
@@ -49,16 +48,12 @@ include('includes/fonctions.php')
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.4/css/buttons.bootstrap4.min.css">
 
-    
+
 </head>
 
 <body>
-    <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-white portfolio-navbar gradient"
-        style="border-bottom-style: solid;border-bottom-color: rgba(0,0,0,0.16);text-shadow: 0px 0px 3px rgb(0,0,0);background: linear-gradient(87deg, rgb(92, 214, 230), rgb(151, 65, 236));">
-        <div class="container"><a class="navbar-brand logo" data-bs-hover-animate="bounce" href="#"
-                style="font-family: 'Alegreya Sans SC', sans-serif;">UGE MANAGER</a><button data-toggle="collapse"
-                class="navbar-toggler" data-target="#navbarNav"><span class="sr-only">Toggle navigation</span><span
-                    class="navbar-toggler-icon"></span></button>
+    <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-white portfolio-navbar gradient" style="border-bottom-style: solid;border-bottom-color: rgba(0,0,0,0.16);text-shadow: 0px 0px 3px rgb(0,0,0);background: linear-gradient(87deg, rgb(92, 214, 230), rgb(151, 65, 236));">
+        <div class="container"><a class="navbar-brand logo" data-bs-hover-animate="bounce" href="#" style="font-family: 'Alegreya Sans SC', sans-serif;">UGE MANAGER</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navbarNav"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="nav navbar-nav ml-auto">
                     <li class="nav-item"><a class="nav-link" data-bs-hover-animate="pulse" href="index.html">Se
@@ -79,10 +74,8 @@ include('includes/fonctions.php')
                             <div class="skills portfolio-info-card">
                                 <h2>Tableau des remises</h2>
                                 <div class="row">
-                                    <div class="skills portfolio-info-card"
-                                        style="width: 100%;margin-bottom: 15px;padding: 25px;">
-                                        <p class="text-uppercase text-left text-dark"
-                                            style="margin: 0;text-align: left;font-weight: bold;width: 100%;float: left;">
+                                    <div class="skills portfolio-info-card" style="width: 100%;margin-bottom: 15px;padding: 25px;">
+                                        <p class="text-uppercase text-left text-dark" style="margin: 0;text-align: left;font-weight: bold;width: 100%;float: left;">
                                             <span style="text-decoration: underline;">RECHERCHE DE REMISES</span></p>
                                         <div class="table-responsive">
                                             <table class="table">
@@ -90,23 +83,21 @@ include('includes/fonctions.php')
                                                     <tr></tr>
                                                 </thead>
                                                 <tbody>
-                                                	<form method="post">
-                                                    <tr>
-                                                        <td><label>N° SIREN :&nbsp;</label><input type="text" name="siren"></td>
-                                                        <td><label style="font-style: normal;" name="raison">Raison sociale
-                                                                :&nbsp;<input type="text"></label></td>
-                                                        <td></td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td><label>Date de début :&nbsp;</label><input type="date" name="datedebut"></td>
-                                                        <td><label>Date de fin :&nbsp;</label><input type="date" name="datefin"></td>
-                                                        <td style="text-align: center;"><button type="sumbit" class="btn btn-primary"
-                                                                type="button"
-                                                                style="text-align: center;background: rgba(255,255,255,0);color: rgb(0,0,0);box-shadow: 0px 0px 3px;border-style: none;">Rechercher</button>
-                                                        </td>
-                                                    </tr>
-                                                    <tr></tr>
-                                                </form>
+                                                    <form method="post">
+                                                        <tr>
+                                                            <td><label>N° SIREN :&nbsp;</label><input type="text" name="siren"></td>
+                                                            <td><label style="font-style: normal;" name="raison">Raison sociale
+                                                                    :&nbsp;<input type="text"></label></td>
+                                                            <td></td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td><label>Date de début :&nbsp;</label><input type="date" name="datedebut"></td>
+                                                            <td><label>Date de fin :&nbsp;</label><input type="date" name="datefin"></td>
+                                                            <td style="text-align: center;"><button type="sumbit" class="btn btn-primary" type="button" style="text-align: center;background: rgba(255,255,255,0);color: rgb(0,0,0);box-shadow: 0px 0px 3px;border-style: none;">Rechercher</button>
+                                                            </td>
+                                                        </tr>
+                                                        <tr></tr>
+                                                    </form>
                                                 </tbody>
                                             </table>
                                         </div>
@@ -115,90 +106,93 @@ include('includes/fonctions.php')
 
                                 <div class="row">
                                     <div class="col">
-                                         <table id="datatable" class="table table-striped table-bordered">
-                    <thead>
-                        <tr>
-                            <th>N° SIREN</th>
-                            <th>Raison sociale</th>
-                            <th>N° remise</th>
-                            <th>Date traitement</th>
-                            <th>Nb transactions</th>
-                            <th>Devise</th>
-                            <th>Montant total</th>
-                            <th>Sens</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
+                                        <table id="datatable" class="table table-striped table-bordered" width="100%">
+                                            <thead>
+                                                <tr>
+                                                    <th>N° SIREN</th>
+                                                    <th>Raison sociale</th>
+                                                    <th>N° remise</th>
+                                                    <th>Date traitement</th>
+                                                    <th>Nb transactions</th>
+                                                    <th>Devise</th>
+                                                    <th>Montant total</th>
+                                                    <th>Sens</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php
 
-                        $id = $_SESSION["logged"];
-                        $requete = "SELECT * FROM REMISE,DEVISE ";
+                                                $id = $_SESSION["logged"];
+                                                $requete = "SELECT * FROM REMISE,DEVISE ";
 
-                        if (isset($_POST['siren'])) $siren = secure_sqlformat($_POST['siren']);
-                        if (isset($_POST['raison'])) $raison = secure_sqlformat(strip_tags($_POST['raison']));
-                        if (isset($_POST['datedebut']))  $datedebut = $_POST['datedebut'];
-                        if (isset($_POST['datefin']))  $datefin = $_POST['datefin'];
+                                                if (isset($_POST['siren'])) $siren = secure_sqlformat($_POST['siren']);
+                                                if (isset($_POST['raison'])) $raison = secure_sqlformat(strip_tags($_POST['raison']));
+                                                if (isset($_POST['datedebut']))  $datedebut = $_POST['datedebut'];
+                                                if (isset($_POST['datefin']))  $datefin = $_POST['datefin'];
 
-                        if ((!empty($siren)) || (!empty($raison)) || (!empty($datedebut)) || (!empty($datefin))){ $requete .= "WHERE";} else { $requete.=" WHERE id_client=$id AND REMISE.id_devise = DEVISE.id_devise";} // si un des champs du formulaire est remplis on met un WHERE
+                                                if ((!empty($siren)) || (!empty($raison)) || (!empty($datedebut)) || (!empty($datefin))) {
+                                                    $requete .= "WHERE";
+                                                } else {
+                                                    $requete .= " WHERE id_client=$id AND REMISE.id_devise = DEVISE.id_devise";
+                                                } // si un des champs du formulaire est remplis on met un WHERE
 
-                        if (!empty($siren)) {
-                            $requete .= " REMISE.siren = '$siren'";
-                            if ((!empty($raison))  || (!empty($datedebut)) || (!empty($datefin))) $requete .= " AND"; // si le champ raison ou date existe on ajoute un AND
-                        }
-                        if (!empty($raison)) {
-                            $requete .= " REMISE.raison LIKE '%$raison%'";
-                            if (!empty($date)) $requete .= " AND"; // si le champ date existe on ajoute un AND
-                        }
-                        if (!empty($datedebut) && empty($datefin)) {
-                            $requete .= " REMISE.date_traitement >= '$datedebut'";
-                        }
-                        if (!empty($datefin) && empty($datedebut)) {
-                            $requete .= " REMISE.date_traitement <= '$datefin'";
-                        }
-                        if (!empty($datefin) && !empty($datedebut)) {
-                            $requete .= " REMISE.date_traitement <= '$datefin' AND REMISE.date_traitement >= '$datedebut'";
-                        }
+                                                if (!empty($siren)) {
+                                                    $requete .= " REMISE.siren = '$siren'";
+                                                    if ((!empty($raison))  || (!empty($datedebut)) || (!empty($datefin))) $requete .= " AND"; // si le champ raison ou date existe on ajoute un AND
+                                                }
+                                                if (!empty($raison)) {
+                                                    $requete .= " REMISE.raison LIKE '%$raison%'";
+                                                    if (!empty($date)) $requete .= " AND"; // si le champ date existe on ajoute un AND
+                                                }
+                                                if (!empty($datedebut) && empty($datefin)) {
+                                                    $requete .= " REMISE.date_traitement >= '$datedebut'";
+                                                }
+                                                if (!empty($datefin) && empty($datedebut)) {
+                                                    $requete .= " REMISE.date_traitement <= '$datefin'";
+                                                }
+                                                if (!empty($datefin) && !empty($datedebut)) {
+                                                    $requete .= " REMISE.date_traitement <= '$datefin' AND REMISE.date_traitement >= '$datedebut'";
+                                                }
 
-                        $requete.=" AND id_client=$id AND REMISE.id_devise = DEVISE.id_devise";
+                                                $requete .= " AND id_client=$id AND REMISE.id_devise = DEVISE.id_devise";
 
-                        $resultat = $db->query($requete);
+                                                $resultat = $db->query($requete);
 
-                        while ($r = $resultat->fetch()){
-                                                            echo '<tr>
+                                                while ($r = $resultat->fetch()) {
+                                                    echo '<tr>
                                                             <td>' . $r['siren'] . '</td>
                                                             <td>' . $r['raison'] . '</td>
                                                             <td>' . $r['num_remise'] . '</td>
-                                                            <td>' . date_format(date_create($r['date_traitement']),"d/m/yy") . '</td>
+                                                            <td>' . date_format(date_create($r['date_traitement']), "d/m/yy") . '</td>
                                                             <td>' . $r['nbr_transaction'] . '</td>
                                                             <td>' . $r['libelle_devise'] . '</td>
                                                             <td>' . $r['montant_remise'] . '</td>
                                                             <td>' . $r['sens'] . '</td>
                                                             </tr>';
-                                                        }
-                        ?>
-                    </tbody>
-                </table>
-                <script type=" text/javascript" charset="utf8" src="assets/js/tableplugins.js">
-    </script>
-                                        </div>
+                                                }
+                                                ?>
+                                            </tbody>
+                                        </table>
+                                        <script type=" text/javascript" charset="utf8" src="assets/js/tableplugins.js">
+                                        </script>
+
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="hobbies group">
-                    <div class="heading"></div>
-                </div>
+            </div>
+            <div class="hobbies group">
+                <div class="heading"></div>
+            </div>
             </div>
         </section>
     </main>
     <footer class="page-footer">
         <div class="container">
             <div class="links"><a href="#">A propos</a><a href="#">Contactez-nous</a></div>
-            <div class="social-icons"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i
-                        class="icon ion-social-instagram-outline"></i></a><a href="#"><i
-                        class="icon ion-social-twitter"></i></a></div>
+            <div class="social-icons"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-instagram-outline"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a></div>
         </div>
     </footer>
 </body>
