72df93b162aed9e06a221a03e5555f2fa7f77643
cydaw6 <cydaw6@gmail.com>
2020-10-03 00:18:04 +0200

    détails et favicon

--- tresorerie.php
+++ tresorerie.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>CV - UGE</title>
+    <title>Portail</title>
+    <link rel="icon" href="assets/img/favicon2.png">
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alegreya+Sans+SC">
@@ -14,12 +15,8 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-white portfolio-navbar gradient"
-        style="border-bottom-style: solid;border-bottom-color: rgba(0,0,0,0.16);text-shadow: 0px 0px 3px rgb(0,0,0);background: linear-gradient(87deg, rgb(92, 214, 230), rgb(151, 65, 236));">
-        <div class="container"><a class="navbar-brand logo" data-bs-hover-animate="bounce" href="#"
-                style="font-family: 'Alegreya Sans SC', sans-serif;">UGE MANAGER</a><button data-toggle="collapse"
-                class="navbar-toggler" data-target="#navbarNav"><span class="sr-only">Toggle navigation</span><span
-                    class="navbar-toggler-icon"></span></button>
+    <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-white portfolio-navbar gradient" style="border-bottom-style: solid;border-bottom-color: rgba(0,0,0,0.16);text-shadow: 0px 0px 3px rgb(0,0,0);background: linear-gradient(87deg, rgb(92, 214, 230), rgb(151, 65, 236));">
+        <div class="container"><a class="navbar-brand logo" data-bs-hover-animate="bounce" href="#" style="font-family: 'Alegreya Sans SC', sans-serif;">PORTAIL-DPCB</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navbarNav"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="nav navbar-nav ml-auto">
                     <li class="nav-item"><a class="nav-link" data-bs-hover-animate="pulse" href="index.html">Se
@@ -39,47 +36,28 @@
                         <div class="col">
                             <div class="skills portfolio-info-card">
                                 <h2>Résumé du compte</h2>
-                                <p class="text-uppercase text-center text-success border-success" data-toggle="tooltip"
-                                    data-bs-tooltip="" title="Votre solde en ce jour"
-                                    style="font-size: 40px;text-shadow: 0px 0px 4px rgb(150,150,150);">+300,00€</p>
+                                <p class="text-uppercase text-center text-success border-success" data-toggle="tooltip" data-bs-tooltip="" title="Votre solde" style="font-size: 40px;text-shadow: 0px 0px 4px rgb(150,150,150);">+300,00€</p>
                                 <p class="text-center" style="height: 23px;margin-top: -24px;font-size: 14px;"><em>Votre
-                                        solde actuel</em><br></p>
+                                        solde</em><br></p>
                                 <div class="row">
                                     <div class="col" style="text-align: center;">
-                                        <div class="btn-group open" style="border-style: none;"><button
-                                                class="btn btn-primary" type="button"
-                                                style="background: rgb(255,255,255);color: rgb(0,0,0);border: 1px solid rgb(0,0,0) ;">Actions</button><button
-                                                class="btn btn-primary dropdown-toggle dropdown-toggle-split"
-                                                data-toggle="dropdown" aria-expanded="true" type="button"
-                                                style="background: rgb(255,255,255);color: rgb(0,0,0);border-style: solid;border-color: rgb(0,0,0);"></button>
+                                        <div class="btn-group open" style="border-style: none;"><button class="btn btn-primary" type="button" style="background: rgb(255,255,255);color: rgb(0,0,0);border: 1px solid rgb(0,0,0) ;">Actions</button><button class="btn btn-primary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-expanded="true" type="button" style="background: rgb(255,255,255);color: rgb(0,0,0);border-style: solid;border-color: rgb(0,0,0);"></button>
                                             <div class="dropdown-menu"><a class="dropdown-item" href="#">Voir les
                                                     impayés</a><a class="dropdown-item" href="#">Voir le résumé de
-                                                    toutes les transactions</a><a class="dropdown-item"
-                                                    href="#">Autre</a></div>
+                                                    toutes les transactions</a><a class="dropdown-item" href="#">Autre</a></div>
                                         </div>
                                     </div>
                                 </div>
-                                <h1
-                                    style="font-size: 20px;border-width: 1px;border-style: none;border-bottom-style: dotted;">
+                                <h1 style="font-size: 20px;border-width: 1px;border-style: none;border-bottom-style: dotted;">
                                     Vos dernières transactions</h1>
                                 <div class="row">
-                                    <div class="skills portfolio-info-card"
-                                        style="width: 100%;margin-bottom: 15px;padding: 25px;">
-                                        <p class="text-uppercase text-left text-dark" data-toggle="tooltip"
-                                            data-bs-tooltip=""
-                                            style="margin: 0;text-align: left;font-weight: bold;width: 80%;float: left;"
-                                            title="Organisme"><span style="text-decoration: underline;">CRÉDIT AGRICOLE
-                                                SEPA 84415641896748</span></p><span class="text-right text-success"
-                                            style="width: 20%;float: right;">+300,00€</span>
+                                    <div class="skills portfolio-info-card" style="width: 100%;margin-bottom: 15px;padding: 25px;">
+                                        <p class="text-uppercase text-left text-dark" data-toggle="tooltip" data-bs-tooltip="" style="margin: 0;text-align: left;font-weight: bold;width: 80%;float: left;" title="Organisme"><span style="text-decoration: underline;">CRÉDIT AGRICOLE
+                                                SEPA 84415641896748</span></p><span class="text-right text-success" style="width: 20%;float: right;">+300,00€</span>
                                     </div>
-                                    <div class="skills portfolio-info-card"
-                                        style="width: 100%;padding: 25px;margin-bottom: 15px;">
-                                        <p class="text-uppercase text-left text-dark" data-toggle="tooltip"
-                                            data-bs-tooltip=""
-                                            style="margin: 0;text-align: left;font-weight: bold;width: 80%;float: left;"
-                                            title="Organisme"><span style="text-decoration: underline;">INTérets année
-                                                2020</span></p><span class="text-right text-danger"
-                                            style="width: 20%;float: right;">+0,00€</span>
+                                    <div class="skills portfolio-info-card" style="width: 100%;padding: 25px;margin-bottom: 15px;">
+                                        <p class="text-uppercase text-left text-dark" data-toggle="tooltip" data-bs-tooltip="" style="margin: 0;text-align: left;font-weight: bold;width: 80%;float: left;" title="Organisme"><span style="text-decoration: underline;">INTérets année
+                                                2020</span></p><span class="text-right text-danger" style="width: 20%;float: right;">+0,00€</span>
                                     </div>
                                 </div>
                             </div>
@@ -95,9 +73,7 @@
     <footer class="page-footer">
         <div class="container">
             <div class="links"><a href="#">A propos</a><a href="#">Contactez-nous</a></div>
-            <div class="social-icons"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i
-                        class="icon ion-social-instagram-outline"></i></a><a href="#"><i
-                        class="icon ion-social-twitter"></i></a></div>
+            <div class="social-icons"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-instagram-outline"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a></div>
         </div>
     </footer>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
