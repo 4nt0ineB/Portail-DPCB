bc083ffe039d3906549ece8298396f9da7526aba
cydaw6 <cydaw6@gmail.com>
2020-10-18 15:19:01 +0200

    gestion des comptes non terminé

--- admin.php
+++ admin.php
@@ -16,7 +16,41 @@ include('includes/fonctions.php')
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Portail</title>
     <?php require_once("includes/head.php"); ?>
+    <style>
+        form.style {
+            padding: none;
+            max-width: none;
+            box-shadow: none;
+            padding: none;
+            margin: none;
+            padding: none;
+        }
 
+        .portfolio-block form {
+            padding: 0px !important;
+        }
+
+        .portfolio-block form {
+            max-width: none;
+            padding: none;
+            margin: auto;
+            box-shadow: none;
+        }
+
+        @media (min-width: 768px) {
+
+            .portfolio-block form {
+                padding: 0px !important;
+            }
+
+            .portfolio-block form {
+                max-width: none;
+                padding: none;
+                margin: none;
+                box-shadow: none;
+            }
+        }
+    </style>
 </head>
 
 <body>
@@ -30,7 +64,16 @@ include('includes/fonctions.php')
                     <div class="row">
                         <div class="col">
                             <div class="skills portfolio-info-card">
-                                <h2>Création de compte</h2>
+                                <?php 
+                                $statusModify = isset($_POST['id_modify_client']); 
+                                $statusDelete = isset($_POST['id_delete_client']);
+                                
+                                ?>
+                                <h2><?php if ($statusModify) echo "Modification de compte";
+                                    elseif ($statusDelete) {
+                                        echo "Suppression de compte";
+                                    }
+                                    else echo  "Création de compte"; ?></h2>
                                 <div class="row">
                                     <div class="skills portfolio-info-card" style="width: 100%;margin-bottom: 15px;padding: 25px;">
                                         <p class="text-uppercase text-left text-dark" style="margin: 0;text-align: left;font-weight: bold;width: 100%;float: left;">
@@ -42,20 +85,83 @@ include('includes/fonctions.php')
                                                 </thead>
                                                 <tbody>
                                                     <tr>
-                                                        <form method="post" name="creation">
 
-                                                            <td><label style="font-style: normal;">Username :
-                                                                    &nbsp;</label><input type="text" name="username"></td>
-                                                            <td><label style="font-style: normal;">Mot de passe :
-                                                                    &nbsp;</label><input type="text" name="mdp"></td>
-                                                            <td><label>N° SIREN :&nbsp;</label><input type="text" name="siren"></td>
-                                                            <td><label style="font-style: normal;">Raison sociale :
-                                                                    &nbsp;<input type="text" name="raison"></label></td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td style="text-align: center;"><button class="btn btn-primary" type="submit" style="text-align: center;background: rgba(255,255,255,0);color: rgb(0,0,0);box-shadow: 0px 0px 3px;border-style: none;">Créer</button>
-                                                        </td>
-                                                        </form>
+                                                        <?php
+
+                                                        if (!($statusDelete)) {
+                                                            if($statusModify){
+                                                                $id_modify_client = $_POST['id_modify_client'];
+                                                                $udata = $db->query("SELECT * FROM `CLIENT` NATURAL JOIN `USER` WHERE `permission` = 1 AND id_client = $id_modify_client")->fetch();
+                                                                
+                                                            }
+                                                           
+                                                            ?>
+                                                            <form method="post" name="creation">
+
+                                                                <td>
+                                                                    <label style="font-style: normal;">Username :&nbsp;</label>          
+                                                                    <input type="text" name="username" value=<?php if ($statusModify) echo $udata["username"] . ""; ?> >
+                                                                </td>
+                                                                <td>
+                                                                    <label style="font-style: normal;">Mot de passe :&nbsp;</label>
+                                                                    <input type="text" name="mdp"></td>
+                                                                <td>
+                                                                    <label>N° SIREN :&nbsp;</label>
+                                                                    <input type="text" name="siren" value=<?php if ($statusModify) echo $udata["siren"] . ""; ?> >
+                                                                </td>
+                                                                <td>
+                                                                    <label style="font-style: normal;">Raison sociale :&nbsp;</label>
+                                                                    <input type="text" name="raison" value=<?php if ($statusModify) echo $udata["raison"] . ""; ?> > <!-- Problème ?! raison coupé -->
+                                                                </td>
+                                                                </tr>
+                                                                <tr>
+                                                                <td style="text-align: center;">
+                                                                    <button class="btn btn-primary" type="submit" style="text-align: center;background: rgba(255,255,255,0);color: rgb(0,0,0);box-shadow: 0px 0px 3px;border-style: none;">
+                                                                        <?php if ($statusModify) echo "Modifier"; else echo "Créer"; ?>
+                                                                    </button>
+                                                            </td>
+                                                            </form>
+                                                            <?php
+                                                        }elseif($statusDelete){
+                                                            $id_modify_client = $_POST['id_delete_client'];
+                                                            $udata = $db->query("SELECT * FROM `CLIENT` NATURAL JOIN `USER` WHERE `permission` = 1 AND id_client = $id_modify_client")->fetch();
+                                                            ?>
+                                                            <form method="post" name="creation">
+
+                                                                <td>
+                                                                    <label style="font-style: normal;">Suppression du compte :&nbsp;</label>          
+                                                                </td>
+                                                                <td>
+                                                                    <label style="font-style: normal;">Username :&nbsp;</label>          
+                                                                    <input type="text" name="username" disabled value=<?php if ($statusDelete) echo $udata["username"] . ""; ?> >
+                                                                </td>
+                                                                
+                                                                <td>
+                                                                    <label>N° SIREN :&nbsp;</label>
+                                                                    <input type="text" name="siren" disabled value=<?php if ($statusDelete) echo $udata["siren"] . ""; ?> ></td>
+                                                                <td>
+                                                                    <label style="font-style: normal;">Raison sociale :&nbsp;</label>
+                                                                    <input type="text" name="raison" disabled value=<?php if ($statusDelete) echo $udata["raison"] . ""; ?> > <!-- Problème ?! raison coupé -->
+                                                                </td>
+                                                                </tr>
+                                                                <tr>
+                                                                <td>
+                                                                    <label style="font-style: normal;">La demande de suppression sera associé à votre nom et le product owner devra la valider:&nbsp;</label>          
+                                                                </td>
+                                                                <td style="text-align: center;">
+                                                                
+                                                                    <button class="btn btn-primary" type="submit" style="text-align: center;background: rgba(255,255,255,0);color: rgb(0,0,0);box-shadow: 0px 0px 3px;border-style: none;">
+                                                                        Supprimer
+                                                                    </button>
+                                                            </td>
+                                                            </form>
+                                                            <?php
+                                                        }
+
+
+                                                        ?>
+
+                                                        
                                                     </tr>
                                                     <tr></tr>
 
@@ -71,25 +177,69 @@ include('includes/fonctions.php')
 
                                 ?>
 
-                                <table id="" class="table table-striped table-bordered" width="100%">
+                                <table id="datatable" class="table table-striped table-bordered" width="100%">
                                     <thead>
                                         <tr>
                                             <th>id_client</th>
                                             <th>N° SIREN</th>
                                             <th>Raison sociale</th>
                                             <th>Nom d'utilisateur</th>
-                                            <th>Mot de passe</th>
+                                            <th>Action</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         <?php
                                         while ($r = $resultat->fetch()) {
-                                            echo '<tr>
-                                                      <td>' . $r['id_client'] . '</td>
+                                            
+
+                                            
+                                            $h = '';
+                                            if($statusModify){
+                                                if($r['id_client'] == $_POST['id_modify_client']){
+                                                    $h = 'style="background-color: orange;"';
+                                                }
+                                            }
+                                            elseif($statusDelete){
+                                                if($r['id_client'] == $_POST['id_delete_client']){
+                                                    $h = 'style="background-color: red;"';
+                                                }
+                                            }
+
+
+                                            echo '<tr '.$h.'><td>' . $r['id_client'] . '</td>
                                                       <td>' . $r['siren'] . '</td>
                                                       <td>' . $r['raison'] . '</td>
                                                       <td>' . $r['username'] . '</td>
-                                                      <td>' . $r['password'] . '</td>';
+                                                      <td>
+
+                                                      <div class="row">
+                                                        <div class="col-2">
+                                                            <form id="formModify" method="POST" action="">
+                                                            <input type="hidden" name="id_modify_client" value="' . $r['id_client'] . '" >
+                                                            <button type="submit" style="border: none; background: none;">
+                                                                <svg width="1.2em" height="1.2em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="black" xmlns="http://www.w3.org/2000/svg">
+                                                                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                                                    <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                                                                </svg>
+
+                                                            </button>
+                                                            </form>
+                                                        </div>
+                                                        <div class="col-2">
+                                                            <form id="formDelete" method="POST" action="">
+                                                            <input type="hidden" name="id_delete_client" value="' . $r['id_client'] . '" >
+                                                            <button type="submit" style="border: none; background: none;">
+                                                                <svg width="1.2em" height="1.2em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="black" xmlns="http://www.w3.org/2000/svg">
+                                                                    <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
+                                                                </svg>
+                                                            </button>
+                                                              
+                                                            </form>
+                                                        </div>
+                                                    </div>
+
+
+                                                      ';
                                         }
                                         ?>
 
